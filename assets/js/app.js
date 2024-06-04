27c4a94b98d8ff0716b666814b53ddbeba6bf09d
mijam31 <156896989+mijam31@users.noreply.github.com>
2024-06-04 12:29:38 +0200

    HEEEEEEEEEEEEELT færdig

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,10 @@
-let slideIndex = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  var splide = new Splide(".splide", {
+    type: "loop",
+    perPage: 3,
+    perMove: 1,
+    gap: "20px",
+  });
 
-let billedeElementer = [
-  document.getElementById("billede1"),
-  document.getElementById("billede2"),
-  document.getElementById("billede3"),
-];
-
-let billeder = [
-  "/assets/img/IMG_1613.JPG",
-  "/assets/img/krug-diner.jpg",
-  "/assets/img/9JHZN5A.jpg",
-];
-
-function prev() {}
-
-function next() {
-  for (let i = 0; i < billedeElementer.length; i++) {
-    const element = billedeElementer[i];
-
-    if (i + 1 + slideIndex == billedeElementer.length) {
-      element.src = billeder[0];
-      slideIndex = 0;
-    } else element.src = billeder[i + 1 + slideIndex];
-  }
-  slideIndex++;
-}
+  splide.mount();
+});
